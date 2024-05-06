17f9f84460a442da5b8e5ccf32425df5c0264864
Hasan Karami Moheb <hasankarami2020313@gmail.com>
2024-05-06 08:12:54 +0330

    update packages

--- src/Infrastructure/Domic.Infrastructure/Implementations.Domain/Repositories/QueryUnitOfWork.cs
+++ src/Infrastructure/Domic.Infrastructure/Implementations.Domain/Repositories/QueryUnitOfWork.cs
@@ -1,21 +1,22 @@
 using System.Data;
 using Domic.Domain.Commons.Contracts.Interfaces;
+using MongoDB.Driver;
 
 namespace Domic.Infrastructure.Implementations.Domain.Repositories;
 
-public class QueryUnitOfWork : IQueryUnitOfWork
+public class QueryUnitOfWork(MongoClient client) : IQueryUnitOfWork
 {
+    private IClientSessionHandle _clientSession;
+
     public void Transaction(IsolationLevel isolationLevel = IsolationLevel.ReadCommitted)
     {
+        _clientSession = client.StartSession(); //Resource
+        _clientSession.StartTransaction();
     }
 
-    public void Commit()
-    {
-    }
+    public void Commit() => _clientSession?.CommitTransaction();
 
-    public void Rollback()
-    {
-    }
+    public void Rollback() => _clientSession?.AbortTransaction();
 
-    public void Dispose() {}
+    public void Dispose() => _clientSession?.Dispose();
 }
