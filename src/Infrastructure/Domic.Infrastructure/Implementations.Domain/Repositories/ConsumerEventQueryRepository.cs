07840ba8763f434d7962a29ef73292057a3b8be5
Hasan Karami Moheb <hasankarami2020313@gmail.com>
2024-06-07 18:32:03 +0330

    changes

--- src/Infrastructure/Domic.Infrastructure/Implementations.Domain/Repositories/ConsumerEventQueryRepository.cs
+++ src/Infrastructure/Domic.Infrastructure/Implementations.Domain/Repositories/ConsumerEventQueryRepository.cs
@@ -1,20 +1,21 @@
 ﻿using Domic.Core.Domain.Contracts.Interfaces;
 using Domic.Core.Domain.Entities;
+using Domic.Persistence.Models;
 using MongoDB.Driver;
 
 namespace Domic.Infrastructure.Implementations.Domain.Repositories.Q;
 
 public class ConsumerEventQueryRepository : IConsumerEventQueryRepository
 {
-    private readonly IMongoCollection<ConsumerEventQuery> _collection;
+    private readonly IMongoCollection<ConsumerEventQueryModel> _collection;
     
     public ConsumerEventQueryRepository(MongoClient mongoClient) 
         => _collection =
-            mongoClient.GetDatabase("StateTrackerService").GetCollection<ConsumerEventQuery>("ConsumerEventQuery");
+            mongoClient.GetDatabase("StateTrackerService").GetCollection<ConsumerEventQueryModel>("ConsumerEventQuery");
     
     public ConsumerEventQuery FindById(object id)
     {
-        var result = _collection.FindSync(query => query.Id == id as string);
+        var result = _collection.FindSync(query => query.MessageId == id as string);
 
         if (result.First() is null) return null;
 
@@ -24,12 +25,23 @@ public class ConsumerEventQueryRepository : IConsumerEventQueryRepository
     public async Task<ConsumerEventQuery> FindByIdAsync(object id, CancellationToken cancellationToken)
     {
         var result =
-            await _collection.FindAsync(query => query.Id == id as string, cancellationToken: cancellationToken);
+            await _collection.FindAsync(query => query.MessageId == id as string, cancellationToken: cancellationToken);
 
         if (result.First() is null) return null;
 
         return new();
     }
 
-    public void Add(ConsumerEventQuery entity) => _collection.InsertOne(entity);
+    public void Add(ConsumerEventQuery entity)
+    {
+        var newModel = new ConsumerEventQueryModel {
+            MessageId = entity.Id,
+            Type = entity.Type,
+            CountOfRetry = entity.CountOfRetry,
+            CreatedAt_EnglishDate = entity.CreatedAt_EnglishDate,
+            CreatedAt_PersianDate = entity.CreatedAt_PersianDate
+        };
+        
+        _collection.InsertOne(newModel);
+    }
 }
